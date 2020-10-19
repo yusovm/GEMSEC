564c2c1424bb790c230b484880c76f2eae99e0bf
yusovm <yusovm@uw.edu>
2020-10-18 17:32:21 -0700

    added wavelet code and updated DMDPlots

--- DMD/DMDWavelet.py
+++ DMD/DMDWavelet.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb  5 12:59:01 2020
+Created on Wed Jul  1 09:46:10 2020
 
 @author: micha
 """
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import pywt
+from pywt import wavedec
 from MD_Analysis import Angle_Calc
 
 #Import pdb files and group them by 50ps/100ps
@@ -58,11 +60,75 @@ for i in np.linspace(0,39,40):
     ft[int(i)]=(ft[int(i)]-np.mean(ft[int(i)]))/np.std(ft[int(i)])
 f=np.transpose(ft)
 
+#Extend matrix size with last data point repeated
+for i in range(f.shape[0]):
+    index=i
+    if 2**index>f.shape[0]:
+        break 
+fadd=np.zeros((2**index-int(f.shape[0]), int(f.shape[1])))
+for i in range(fadd.shape[0]):
+    fadd[i]=f[-1]
+fwav=np.vstack((f,fadd))
+
+#Compute wavelet transform
+(cA, cD)=pywt.dwt(fwav, 'db1', axis=0)
+cA.shape
+cD.shape
+coeffs = wavedec(fwav, 'bior6.8', level=15, axis=0)
+
+#Separate coefficients by different frequencies, run DMD
+freq=[0]*index
+freq[0]=cA
+for i in np.linspace(1,index-1,index-1):
+    freq[int(i)]=cD[int(cD.shape[0]*(2**(i-1)-1)/(2**(i-1))):int(cD.shape[0]*(2**(i)-1)/(2**(i))),:]
+wavDMD=[0]*index
+for i in range(0,index):
+    if freq[i].shape[0]>40:
+        wavDMD[i]=DMD(1,freq[i],40)
+    else:
+        wavDMD[i]=DMD(1,freq[i],freq[i].shape[0]-1)
+
+#Reconstruct cA, cD, and compute inverse transform and average RMSE
+cAinv=wavDMD[0].T
+cDinv=np.concatenate([x.T for x in wavDMD[1:]], axis = 0)
+cDinv=np.vstack((cDinv,cDinv[-1]))
+invf=pywt.idwt(cAinv,cDinv,'db1',axis=0)
+error=np.mean(np.sqrt((invf-fwav)**2))
+
+wavDMD=[0]*index
+coeffs = wavedec(fwav, 'bior1.1', level=index-1, axis=0)
+for i in range(0,index):
+    if coeffs[i].shape[0]>40:
+        wavDMD[i]=DMD(1,coeffs[i],40).T
+    else:
+        wavDMD[i]=DMD(1,coeffs[i],coeffs[i].shape[0]-1).T
+invf=pywt.waverec(wavDMD, 'bior1.1',axis=0)
+error=np.mean(np.sqrt((invf-fwav)**2))
+error
+
+coeffs={}
+wavDMD={}
+error={}
+for wavname in pywt.wavelist(kind='discrete'):
+    print('Working on ' + wavname)
+    coeffs[wavname] = wavedec(fwav, wavname, level=index-1, axis=0)
+    try:
+        wavDMD[wavname]=[0]*index
+        for i in range(0,index):
+            if coeffs[wavname][i].shape[0]>40:
+                wavDMD[wavname][i]=DMD(1,coeffs[wavname][i],40).T
+            else:
+                wavDMD[wavname][i]=DMD(1,coeffs[wavname][i],coeffs[wavname][i].shape[0]-1).T
+        invf=pywt.waverec(wavDMD[wavname], wavname,axis=0)
+        error[wavname]=np.mean(np.sqrt((invf-fwav)**2))
+    except:
+        print('Exception while working on ' + wavname)
+
 #Define dt based on simulation time step (100ps) and r based on number of columns
 dt=100*(10**-12)
 r=40
 
-#DMD function
+#DMD function (rows<columns)
 def DMD(dt,f,r):
     #Create data matrices X1, X2
     X=f.T
@@ -97,14 +163,14 @@ def DMD(dt,f,r):
     return X_dmd
 
 #Applying DMD with varying amounts of data used and calculating average error
-def timesampled_avg_error(data,x):
+def DMDError_1(data,x):
     f = data[0:x,:]
     X_dmd = DMD(dt,f,r)
     error=np.sqrt((X_dmd-f.T)**2)
     return np.mean(error)
 
 #Applying DMD with varying amounts of data used and reconstructed/predicted and calculating average error
-def variable_timesampled_avg_error(data,x,n):
+def DMDError_2(data,x,n):
     f = data[0:x,:]
     X_dmd = DMD(dt,f,r)
     X_dmd = X_dmd[:,0:n]
@@ -113,7 +179,7 @@ def variable_timesampled_avg_error(data,x,n):
 
 #Applying DMD with varying amounts of data used, reconstructed/predicted, and sparser sampling
 #while changing truncation when less columns than rows and calculating average error
-def variable_timesampledstepsize_avg_error(data,num_timesteps,num_samples_recon,timestep):
+def DMDError_3(data,num_timesteps,num_samples_recon,timestep):
     print(num_timesteps, num_samples_recon, timestep)
     g = data[0:num_timesteps,:]
     sample_indexes = [i % timestep == 0 for i in range(g.shape[0])]
@@ -127,8 +193,18 @@ def variable_timesampledstepsize_avg_error(data,num_timesteps,num_samples_recon,
     error=np.sqrt((X_dmd-g.T[:,0:num_samples_recon])**2)
     return np.mean(error)
     
+#Applying DMD with varying amounts of data used and reconstructed/predicted and calculating average error
+def DMDError_2F(data,x,n):
+    f = data[0:x,:]
+    f_fourier = np.fft.fft(f,axis=0)
+    X_dmd = DMD(dt,f_fourier,r)
+    X_dmd = np.fft.ifft(X_dmd,axis=0)
+    X_dmd = X_dmd[:,0:n]
+    error=np.sqrt((X_dmd-f.T[:,0:n])**2)
+    return np.mean(error)
+
 tstep = [int(x) for x in np.linspace(50,36500,100)]
-error_data = [[variable_timesampled_avg_error(f, i, j) for i in tstep]
+error_data = [[DMDError_2F(f, i, j) for i in tstep]
                                for j in tstep]
 error_plot_data = np.real(np.array(error_data))
 
@@ -146,7 +222,7 @@ error_data = np.zeros((t_num_samples_num,t_num_samples_num,len(t_timestep)))
 for i in range(t_num_samples_num):
     for j in range(t_num_samples_num):
         for k in range(len(t_timestep)):
-            error_data[i,j,k] = variable_timesampledstepsize_avg_error(f, 
+            error_data[i,j,k] = DMDError_3(f, 
                           t_num_samples[i], t_num_samples[j], t_timestep[k])
 np.save("errors_fourier",error_data)
 
@@ -184,14 +260,13 @@ plt.show()
 for angle in range(0, 360):
     ax.view_init(30, angle)
     fig.savefig('ang'+str(angle)+'.png')
-
+    
 def pltsubtitle(i):
     cs = i % 2 == 0
     pp = int(i/2) % 2 == 0
     num = int(i /4)
     return("cos(" if cs else "sin(") + (r"$\phi_{" if pp else r"$\psi_{") + str(num+1) + ("}$)")
     
-
 fig, ax = plt.subplots(10,1,sharex='col',sharey='row',num=None,
                       figsize=(15,10),dpi=100,constrained_layout=True)
 fig.suptitle('Root Mean Squared Error of DMD Predicted Values')
@@ -232,56 +307,58 @@ for i in np.linspace(31,40,10):
     ax[int(i)-31].set_xlabel('Time (s)')
 fig.savefig("Error 4 Norm.png")
 
+t=np.linspace(0,36500,36500)*dt
+
 fig2, ax2 = plt.subplots(10,2,sharex='col', sharey='row', num=None, 
                         figsize=(15, 10), dpi=100, constrained_layout=True)
-fig2.suptitle('Actual vs. DMD Predicted Values')
+fig2.suptitle('Actual vs. Wavelet+DMD Reconstructed Data')
 for i in range(10):
-    ax2[i,0].plot(t, X[i,:], color='brown')
-    ax2[i,1].plot(t, X_dmd[i,:], color='cadetblue')
+    ax2[i,0].plot(t, f[:,i], color='brown')
+    ax2[i,1].plot(t, invf[:,i], color='cadetblue')
     ax2[i,0].set_ylabel(pltsubtitle(i))
     ax2[i,1].set_ylabel(pltsubtitle(i))
     ax2[i,0].set_xlabel('Time (s)')
     ax2[i,1].set_xlabel('Time (s)')
-fig2.savefig("Actual vs. Preidcted 1 Norm.png")
+fig2.savefig("Actual vs. Preidcted Wavelet 1.png")
 plt.show()
 
 fig2, ax2 = plt.subplots(10,2,sharex='col', sharey='row', num=None, 
                         figsize=(15, 10), dpi=100, constrained_layout=True)
-fig2.suptitle('Actual vs. DMD Predicted Values')
+fig2.suptitle('Actual vs. Wavelet+DMD Reconstructed Data')
 for i in np.linspace(11,20,10):
-    ax2[int(i)-11,0].plot(t, X[int(i)-1,:], color='brown')
-    ax2[int(i)-11,1].plot(t, X_dmd[int(i)-1,:], color='cadetblue')
+    ax2[int(i)-11,0].plot(t, f[:,int(i)-1], color='brown')
+    ax2[int(i)-11,1].plot(t, invf[:,int(i)-1], color='cadetblue')
     ax2[int(i)-11,0].set_ylabel(pltsubtitle(int(i)-1))
     ax2[int(i)-11,1].set_ylabel(pltsubtitle(int(i)-1))
     ax2[int(i)-11,0].set_xlabel('Time (s)')
     ax2[int(i)-11,1].set_xlabel('Time (s)')
-fig2.savefig("Actual vs. Preidcted 2 Norm.png")
+fig2.savefig("Actual vs. Preidcted Wavelet 2.png")
 plt.show()
 
 fig2, ax2 = plt.subplots(10,2,sharex='col', sharey='row', num=None, 
                         figsize=(15, 10), dpi=100, constrained_layout=True)
-fig2.suptitle('Actual vs. DMD Predicted Values')
+fig2.suptitle('Actual vs. Wavelet+DMD Reconstructed Data')
 for i in np.linspace(21,30,10):
-    ax2[int(i)-21,0].plot(t, X[int(i)-1,:], color='brown')
-    ax2[int(i)-21,1].plot(t, X_dmd[int(i)-1,:], color='cadetblue')
+    ax2[int(i)-21,0].plot(t, f[:,int(i)-1], color='brown')
+    ax2[int(i)-21,1].plot(t, invf[:,int(i)-1], color='cadetblue')
     ax2[int(i)-21,0].set_ylabel(pltsubtitle(int(i)-1))
     ax2[int(i)-21,1].set_ylabel(pltsubtitle(int(i)-1))
     ax2[int(i)-21,0].set_xlabel('Time (s)')
     ax2[int(i)-21,1].set_xlabel('Time (s)')
-fig2.savefig("Actual vs. Preidcted 3 Norm.png")
+fig2.savefig("Actual vs. Preidcted Wavelet 3.png")
 plt.show()
 
 fig2, ax2 = plt.subplots(10,2,sharex='col', sharey='row', num=None, 
                         figsize=(15, 10), dpi=100, constrained_layout=True)
-fig2.suptitle('Actual vs. DMD Predicted Values')
+fig2.suptitle('Actual vs. Wavelet+DMD Reconstructed Data')
 for i in np.linspace(31,40,10):
-    ax2[int(i)-31,0].plot(t, X[int(i)-1,:], color='brown')
-    ax2[int(i)-31,1].plot(t, X_dmd[int(i)-1,:], color='cadetblue')
+    ax2[int(i)-31,0].plot(t, f[:,int(i)-1], color='brown')
+    ax2[int(i)-31,1].plot(t, invf[:,int(i)-1], color='cadetblue')
     ax2[int(i)-31,0].set_ylabel(pltsubtitle(int(i)-1))
     ax2[int(i)-31,1].set_ylabel(pltsubtitle(int(i)-1))
     ax2[int(i)-31,0].set_xlabel('Time (s)')
     ax2[int(i)-31,1].set_xlabel('Time (s)')
-fig2.savefig("Actual vs. Preidcted 4 Norm.png")
+fig2.savefig("Actual vs. Preidcted Wavelet 4.png")
 plt.show()
 
 plt.axhline(y=0,color='blue',zorder=-1)
